fcaae2d7c0389fc49a88fe38f7995ff1346c63b1
Howard <howardenq@gmail.com>
2018-07-16 00:09:34 -0700

    This is the Code
    Im pretty new to github but have been coding for some time now but This code is super simple and in no means mean if youll get into college or not

---
+++ College apt.py
@@ -0,0 +1,19 @@
+import sklearn
+from sklearn import tree
+
+#[gpa, act, sat]
+
+X = [[2.5,22,1090],[3.6,24,1160],[4.0,36,1600],[2.2,34,1520],[3.3,34,1520],[3.1,29,1360],[3.4,25,1180],
+	[4.0,22,1120],[3.5,20,1030],[3.4,29,1350],[3.7,30,1380]] 
+	
+Y =	['decline', 'accept', 'accept','decline', 'accept', 'accept', 'decline','decline', 'decline', 'accept','accept']
+ 
+
+clf = tree.DecisionTreeClassifier()
+
+clf = clf.fit(X,Y)
+
+
+prediction = clf.predict([[3.5,28,1250]])
+
+print(prediction)
